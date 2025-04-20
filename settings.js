90689dcca8ca596e6c1c87d0caaa5f2aefa578c1
pathuma321 <Nimeshapathum51@gmail.com>
2025-04-20 20:50:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || ""
-global.botname = process.env.BOTNAME || ""
-global.ownernumber = process.env.OWNERNUMBER || ""
-global.ownername = process.env.OWNERNAME || ""
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~~~L1pDxDQY#nySxLdIqWCJ5DIN24Bwb9K9Uvu3D5dcIFfVAPxC-p6U"
+global.botname = process.env.BOTNAME || "PATHUM-X"
+global.ownernumber = process.env.OWNERNUMBER || "94785181711"
+global.ownername = process.env.OWNERNAME || "Pathum Nimesh"
 global.websitex = process.env.WEBSITEX || ""
 global.wagc = process.env.WAGC || ""
 global.botscript = process.env.BOTSCRIPT || ""
-global.packname = process.env.PACKNAME || "KAVI-X"
-global.author = process.env.AUTHOR || ""
+global.packname = process.env.PACKNAME || "PATHUM-X"
+global.author = process.env.AUTHOR || "Pathum Nimesh"
 global.creator = process.env.CREATOR || ""
-global.botprefix = process.env.BOTPREFIX || ""
+global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
-global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://kavishka:KAVImihi321@whatsapp-bot.ssmxc2a.mongodb.net/testing?retryWrites=true&w=majority&appName=whatsapp-bot"
+global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://kavishka:KAVImihi321@whatsapp-bot.ssmxc2a.mongodb.net/pathuma?retryWrites=true&w=majority&appName=whatsapp-bot"
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
